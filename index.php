564263069bcda536bdae5e99046443e04f5c5baa
Mariusz Kauczor <aqua18@o2.pl>
2014-11-28 14:21:58 +0100

    Update

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
-Project by
+<?php 
+//Project by https://github.com/roshed
+//Let see config.php
+include('config.php');
+include('view/oop/load.php'); //OOP function
+session_start();
 
-######                              ######  
-#     #  ####   ####  #    # ###### #     # 
-#     # #    # #      #    # #      #     # 
-######  #    #  ####  ###### #####  #     # 
-#   #   #    #      # #    # #      #     # 
-#     #  ####   ####  #    # ###### ######  
+//documents folder
+$first = 'view'; // First folder in sitehell libelary.
+$second = 'layout'; // Second folder in sitehell libelary.
+$three = 'index'; // Third folder in sitehell libelary.
+
+//end navigation
+//Start controller
+$controller = 'index'; //Name project in $three folder, default is "index".
+$top = 'header.php'; // Header file start in doctype, end </head>.
+$bot = 'footer.php'; //Bottom doctype, start <footer> end </html>.
+
+/*
+Core Sitehell
+*/
+$layout = ($first.'/'.$second.'/');
+
+//if get=view is empty, load index.php
+$view = empty($_GET['view']) ? 'index' : $_GET['view']; 
+
+//view to the get
+//example is address.com/view=library
+switch($view) {
+	case 'index' :
+
+	break;
+
+	case 'library' :
+
+	break;
+
+	default:
+       $view='index';
+}
+//And you must add file.php to $first/$three
+//Difoult 
+
+//Load main controller.
+require($first.'/'.$second.'/'.$controller.'.php');
+
+/*
+End-Core Sitehell
+*/
+?>
